b1d49705bb1ed98669abdca240654b5b4a07ed7e
Thomas Saurel <111185572+TomSaw31@users.noreply.github.com>
2025-03-13 18:17:50 +0100

    Fin TP1

--- src/villagegaulois/VillageSansChefException.java
+++ src/villagegaulois/VillageSansChefException.java
@@ -3,7 +3,18 @@ package villagegaulois;
 public class VillageSansChefException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
+	public VillageSansChefException() {
+	}
+
 	public VillageSansChefException(String message) {
 		super(message);
+	}
+
+	public VillageSansChefException(Throwable cause) {
+		super(cause);
+	}
+
+	public VillageSansChefException(String message, Throwable cause) {
+		super(message,cause);
 	}
 }
